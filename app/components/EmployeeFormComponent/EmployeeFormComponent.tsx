6140e85430e4b31e3f1ab380792135733bcfef0a
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-10 23:50:45 +0200

    refactor: use input text component

--- app/components/EmployeeFormComponent/EmployeeFormComponent.tsx
+++ app/components/EmployeeFormComponent/EmployeeFormComponent.tsx
@@ -1,13 +1,11 @@
 import { Form } from "react-router";
 import "./EmployeeFormComponent.css";
+import InputTextComponent from "../InputTextComponent/InputTextComponent";
 
 export default function EmployeeFormComponent() {
   return (
     <Form method="post">
-      <div>
-        <label htmlFor="full_name">Full Name</label>
-        <input type="text" name="full_name" id="full_name" required />
-      </div>
+      <InputTextComponent />
       <button type="submit">Create Employee</button>
     </Form>
   );
--- app/components/InputTextComponent/InputTextComponent.tsx
+++ app/components/InputTextComponent/InputTextComponent.tsx
@@ -1,5 +1,10 @@
 import "./InputTextComponent.css";
 
 export default function InputTextComponent() {
-  return <></>;
+  return (
+    <p>
+      <label htmlFor="full_name">Full Name</label>
+      <input type="text" name="full_name" id="full_name" required />
+    </p>
+  );
 }
