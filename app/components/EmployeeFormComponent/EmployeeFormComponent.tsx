47d1c4c1603f6a83e6a662b0498cf8f0a818af0b
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-10 23:48:39 +0200

    refactor: user employee form component

--- app/components/EmployeeFormComponent/EmployeeFormComponent.tsx
+++ app/components/EmployeeFormComponent/EmployeeFormComponent.tsx
@@ -1,5 +1,14 @@
+import { Form } from "react-router";
 import "./EmployeeFormComponent.css";
 
 export default function EmployeeFormComponent() {
-  return <></>;
+  return (
+    <Form method="post">
+      <div>
+        <label htmlFor="full_name">Full Name</label>
+        <input type="text" name="full_name" id="full_name" required />
+      </div>
+      <button type="submit">Create Employee</button>
+    </Form>
+  );
 }
--- app/routes/employees.new/route.tsx
+++ app/routes/employees.new/route.tsx
@@ -1,4 +1,5 @@
 import { Form, redirect, type ActionFunction } from "react-router";
+import EmployeeFormComponent from "~/components/EmployeeFormComponent/EmployeeFormComponent";
 import NavBarComponent from "~/components/NavBarComponent/NavBarComponent";
 import { getDB } from "~/db/getDB";
 
@@ -22,13 +23,7 @@ export default function NewEmployeePage() {
         ]}
       />
       <h1>Create New Employee</h1>
-      <Form method="post">
-        <div>
-          <label htmlFor="full_name">Full Name</label>
-          <input type="text" name="full_name" id="full_name" required />
-        </div>
-        <button type="submit">Create Employee</button>
-      </Form>
+      <EmployeeFormComponent />
     </div>
   );
 }
