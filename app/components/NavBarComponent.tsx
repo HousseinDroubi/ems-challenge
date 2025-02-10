87a5ddb44a2223c0ca6126a0e364c2ee3fe370fd
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-10 22:35:07 +0200

    add more reusable classes

--- app/components/NavBarComponent.tsx
+++ app/components/NavBarComponent.tsx
@@ -1,8 +1,8 @@
 export default function NavBarComponent({ pages }: any) {
   return (
-    <nav className="nav-bar">
+    <nav className="nav-bar flex j-c-s-b w-100">
       <h3>EMS-Challenge</h3>
-      <ul>
+      <ul className="flex j-c-e">
         {pages.map((page: any) => (
           <li>
             <a href={page.to}>{page.title}</a>
