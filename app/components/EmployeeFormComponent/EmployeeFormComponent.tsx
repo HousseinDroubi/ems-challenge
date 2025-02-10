b1cb2e15a996422192e386aba0b0b9f2d9493a98
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-11 00:48:41 +0200

    feat: add email input text

--- app/components/EmployeeFormComponent/EmployeeFormComponent.tsx
+++ app/components/EmployeeFormComponent/EmployeeFormComponent.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 
 export default function EmployeeFormComponent() {
   const [text, setText] = useState("");
+  const [email, setEmail] = useState("");
 
   return (
     <Form method="post">
@@ -12,6 +13,11 @@ export default function EmployeeFormComponent() {
         text={text}
         type="text"
         onTextChange={(newText: string) => setText(newText)}
+      />
+      <InputTextComponent
+        text={email}
+        type="email"
+        onTextChange={(newText: string) => setEmail(newText)}
       />
       <button type="submit">Create Employee</button>
     </Form>
