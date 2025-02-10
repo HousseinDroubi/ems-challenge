ad7b08c3113e779467187e40667a46a408148ab1
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-10 22:21:55 +0200

    add nav bar page list

--- app/components/NavBarComponent.tsx
+++ app/components/NavBarComponent.tsx
@@ -2,6 +2,13 @@ export default function NavBarComponent({ pages }: any) {
   return (
     <nav className="nav-bar">
       <h2>EMS-Challenge</h2>
+      <ul>
+        {pages.map((page: any) => (
+          <li>
+            <a href={page.to}>{page.title}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
