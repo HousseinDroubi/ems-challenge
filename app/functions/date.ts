71973084fd3f35f48531d960d8de07c74b35646a
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-12 00:12:29 +0200

    feat: implement a function to find if age is 18 or orlder

--- app/functions/date.ts
+++ app/functions/date.ts
@@ -3,4 +3,20 @@ function isValidDate(dateString: string) {
   return !isNaN(date.getTime());
 }
 
-export { isValidDate };
+function is18OrOlder(dateString: string) {
+  const today = new Date();
+  const date = new Date(dateString);
+
+  let age = today.getFullYear() - date.getFullYear();
+
+  const monthDifference = today.getMonth() - date.getMonth();
+  const dayDifference = today.getDate() - date.getDate();
+
+  if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
+    age--; // Decrease the age if birthday hasn't happened yet this year
+  }
+
+  return age >= 18;
+}
+
+export { isValidDate, is18OrOlder };
