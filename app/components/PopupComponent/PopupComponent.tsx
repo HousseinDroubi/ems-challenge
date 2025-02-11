dd283959d084521e119c99caf8c668d51d9962c8
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-11 20:55:28 +0200

    feat: implement popup style

--- app/components/PopupComponent/PopupComponent.tsx
+++ app/components/PopupComponent/PopupComponent.tsx
@@ -1,5 +1,24 @@
 import "./PopupComponent.css";
 
-export default function PopupComponent() {
-  return <></>;
+export default function PopupComponent({ text, is_visible }: any) {
+  return (
+    <div className="popup-container flex j-c-c a-i-c">
+      <main className="flex flex-column">
+        <header className="flex a-i-c mt-10">
+          <div className="flex j-c-c a-i-c">
+            <h3>Attention</h3>
+          </div>
+          <div className="flex j-c-c a-i-c">
+            <h3>X</h3>
+          </div>
+        </header>
+        <main className="flex j-c-c a-i-c">
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam,
+            minima!
+          </h4>
+        </main>
+      </main>
+    </div>
+  );
 }
