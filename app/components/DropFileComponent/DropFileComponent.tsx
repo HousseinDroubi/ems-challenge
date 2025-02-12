a17d574fbcdb344972fc2227918bf66d0fa003e2
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-12 13:51:33 +0200

    feat: implement drag over and drag leave and drop listeners

--- app/components/DropFileComponent/DropFileComponent.tsx
+++ app/components/DropFileComponent/DropFileComponent.tsx
@@ -9,6 +9,17 @@ export default function DropFileComponent({ file_name }: any) {
       <label
         htmlFor="input_file"
         className={`h-100 flex j-c-c a-i-c ${is_hovering ? "hover" : ""}`}
+        onDragOver={(event) => {
+          event.preventDefault();
+          setIsHovering(true);
+        }}
+        onDragLeave={() => {
+          setIsHovering(false);
+        }}
+        onDrop={(event) => {
+          event.preventDefault();
+          setIsHovering(false);
+        }}
       >
         <p>Drag and drop {file_name} here</p>
         <input type="file" accept="image/*" id="input_file" />
