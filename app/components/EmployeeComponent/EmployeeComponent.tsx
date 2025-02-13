4916c95e406ebbbebb0f3c42ab9f36e55841c038
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-13 20:12:55 +0200

    feat: show employee age

--- app/components/EmployeeComponent/EmployeeComponent.tsx
+++ app/components/EmployeeComponent/EmployeeComponent.tsx
@@ -1,3 +1,4 @@
+import { getAgeFromBirthDate } from "~/functions/date";
 import "./EmployeeComponent.css";
 
 export default function EmployeeComponent({ employee }: any) {
@@ -11,6 +12,7 @@ export default function EmployeeComponent({ employee }: any) {
             : employee.full_name}
         </h3>
         <h3>Salary: {employee.salary}$</h3>
+        <h4>Age: {getAgeFromBirthDate(employee.date_of_birth)} years</h4>
         <h4>Job Level: {employee.job_level}</h4>
         <br />
         <h5>Phone Number: {employee.phone_number}</h5>
