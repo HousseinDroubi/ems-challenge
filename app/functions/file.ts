3b5e7ed9ee7cb24bc4a9abd12ced7a17ed6a9560
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-13 06:36:30 +0200

    feat: implement a function to get blob from buffer

--- app/functions/file.ts
+++ app/functions/file.ts
@@ -2,4 +2,12 @@ const convertToBase64 = (image: Buffer) => {
   return `data:image/jpeg;base64,${image.toString("base64")}`;
 };
 
-export { convertToBase64 };
+const getBlobFromBuffer = (buffer: Buffer) => {
+  const array = new Uint8Array(buffer); // Convert Buffer to Uint8Array
+
+  // Create a Blob from the array
+  const blob = new Blob([array], { type: "image/jpeg" });
+  return blob;
+};
+
+export { convertToBase64, getBlobFromBuffer };
