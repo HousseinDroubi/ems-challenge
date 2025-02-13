825fc4fba8c88b488d669e98fa35b2f145f1cce7
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-13 07:06:31 +0200

    feat: implement a function to get image source from blob

--- app/functions/file.ts
+++ app/functions/file.ts
@@ -10,4 +10,12 @@ const getBlobFromBuffer = (buffer: Buffer) => {
   return blob;
 };
 
-export { convertToBase64, getBlobFromBuffer };
+const getImageSrcFromBlob = (image: Blob): string | void => {
+  const reader = new FileReader();
+  reader.onloadend = () => {
+    if ((reader.result as string).split(",")[1] !== "") return reader.result; // Set the image as a data URL
+  };
+  reader.readAsDataURL(image);
+};
+
+export { convertToBase64, getBlobFromBuffer, getImageSrcFromBlob };
