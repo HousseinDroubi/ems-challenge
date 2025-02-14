dce688c4da5e7dcd27c63186987330baf65d6350
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-14 07:25:41 +0200

    feat: implement calendar with fake data

--- app/components/CalendarComponent/CalendarComponent.tsx
+++ app/components/CalendarComponent/CalendarComponent.tsx
@@ -1,5 +1,25 @@
+import { createViewWeek, createViewMonthGrid } from "@schedule-x/calendar";
+import { ScheduleXCalendar, useCalendarApp } from "@schedule-x/react";
+import "@schedule-x/theme-default/dist/index.css";
 import "./CalendarComponent.css";
 
 export default function CalendarComponent() {
-  return <></>;
+  const calendar = useCalendarApp({
+    views: [createViewWeek(), createViewMonthGrid()],
+    // ! Mock Data
+    events: [
+      {
+        id: 1,
+        title: "Title",
+        start: "2025-02-14 08:00",
+        end: "2025-02-14 10:00",
+      },
+    ],
+    selectedDate: "2025-02-13",
+  });
+  return (
+    <>
+      <ScheduleXCalendar calendarApp={calendar} />
+    </>
+  );
 }
