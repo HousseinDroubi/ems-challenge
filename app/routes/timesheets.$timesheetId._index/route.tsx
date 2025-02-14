2fe34e8df5ed6b1503d9f5761cc5152722605ed2
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-14 03:55:12 +0200

    feat: get employees and single timesheet from DB

--- app/routes/timesheets.$timesheetId._index/route.tsx
+++ app/routes/timesheets.$timesheetId._index/route.tsx
@@ -1,7 +1,27 @@
 import NavBarComponent from "~/components/NavBarComponent/NavBarComponent";
+import { getDB } from "~/db/getDB";
 
-export async function loader() {
-  return {};
+export async function loader({ request }: any) {
+  const id = request.url.split("/").pop();
+  const db = await getDB();
+  const timesheetAndEmployees = await db.all(
+    `SELECT employees.id as _employee_id, employees.full_name, timesheets.* FROM employees,timesheets WHERE timesheets.id = ${id}`
+  );
+  const data: any = {};
+  // Get users
+  data.users = timesheetAndEmployees.map((element) => {
+    return {
+      id: element._employee_id,
+      full_name: element.full_name,
+    };
+  });
+  data.timesheet = {
+    id: timesheetAndEmployees[0].id,
+    start_time: timesheetAndEmployees[0].start_time,
+    end_time: timesheetAndEmployees[0].end_time,
+    employee_id: timesheetAndEmployees[0].employee_id,
+  };
+  return { data };
 }
 
 export default function TimesheetPage() {
@@ -14,7 +34,6 @@ export default function TimesheetPage() {
           { to: "/timesheets/new", title: "New Timesheet" },
         ]}
       />
-      <div>To implement</div>
     </div>
   );
 }
