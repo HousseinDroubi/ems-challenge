92eef0cce7b8ac5386e6b6dec2f2424087a35d1b
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-14 05:24:12 +0200

    perf: improve filling default values

--- app/components/TimesheetFormComponent/TimesheetFormComponent.tsx
+++ app/components/TimesheetFormComponent/TimesheetFormComponent.tsx
@@ -8,16 +8,6 @@ export default function TimesheetFormComponent({
   update,
   timesheet,
 }: any) {
-  const [selected_option, setSelectedOption] = useState(0);
-
-  useEffect(() => {
-    if (update) {
-      const index = employees.findIndex(
-        (element: any) => element.id === timesheet.employee_id
-      );
-      setSelectedOption(index);
-    }
-  }, []);
   return (
     <article className="timesheet-container">
       <h1 className="mt-20">
@@ -29,13 +19,9 @@ export default function TimesheetFormComponent({
             <select
               name="employee_id"
               id="employees_input_section"
-              value={selected_option}
-              onChange={(e) => {
-                const index = employees.findIndex((employee: any) => {
-                  return e.target.value == employee.id;
-                });
-                setSelectedOption(index);
-              }}
+              defaultValue={employees.findIndex(
+                (element: any) => element.id === timesheet.employee_id
+              )}
             >
               {employees.map((employee: any) => (
                 <option value={employee.id} key={employee.id}>
@@ -48,8 +34,7 @@ export default function TimesheetFormComponent({
         <div className="mt-10">
           <label htmlFor="start_time">Start Time:</label>
           <input
-            readOnly
-            value={timesheet.start_time}
+            defaultValue={timesheet.start_time}
             type="datetime-local"
             name="start_time"
             id="start_time"
@@ -59,8 +44,7 @@ export default function TimesheetFormComponent({
         <div className="mt-10">
           <label htmlFor="end_time">End Time:</label>
           <input
-            readOnly
-            value={timesheet.end_time}
+            defaultValue={timesheet.end_time}
             type="datetime-local"
             name="end_time"
             id="end_time"
