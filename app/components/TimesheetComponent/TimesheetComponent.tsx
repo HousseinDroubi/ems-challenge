62dcb0d78d8f87e46aac9472b7e2020f69ca7f5f
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-14 10:39:20 +0200

    fix: use table component

--- app/components/TimesheetComponent/TimesheetComponent.tsx
+++ app/components/TimesheetComponent/TimesheetComponent.tsx
@@ -1,6 +1,14 @@
+import TableComponent from "../TableComponent/TableComponent";
 import "./TimesheetComponent.css";
 
 export default function TimesheetComponent({ timesheetAndEmployee }: any) {
   console.log(timesheetAndEmployee);
-  return <h1>hi</h1>;
+  return (
+    <TableComponent
+      employee_id={timesheetAndEmployee.employee_id}
+      timesheet_id={timesheetAndEmployee.id}
+      image={timesheetAndEmployee.image}
+      full_name={timesheetAndEmployee.full_name}
+    ></TableComponent>
+  );
 }
