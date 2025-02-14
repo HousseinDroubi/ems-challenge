5332a93248ec2c77f2728761a7b97a31337c66fe
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-14 22:04:16 +0200

    feat: add choices for filter by

--- app/routes/employees._index/route.tsx
+++ app/routes/employees._index/route.tsx
@@ -22,6 +22,7 @@ export async function loader() {
   });
   return { employees };
 }
+
 export const action: ActionFunction = async ({ request }) => {
   const db = await getDB();
   const formData = await request.formData();
@@ -40,6 +41,9 @@ export const action: ActionFunction = async ({ request }) => {
   }
 };
 export default function EmployeesPage() {
+  const [option_filter_by, setOptionFilterBy] = useState<any>("full_name");
+  const [options_filter, setOptionsFilter] = useState<any>([]);
+
   const { employees } = useLoaderData();
   const data = useActionData();
   const [search_bar_text, setSearchBarText] = useState("");
@@ -47,6 +51,75 @@ export default function EmployeesPage() {
     filtered_times_and_employees_with_images,
     setFilteredTimesheetsAndEmployeesWithImages,
   ] = useState([]);
+
+  useEffect(() => {
+    switch (option_filter_by) {
+      case "full_name":
+        setOptionsFilter([
+          {
+            content: "> 30",
+            value: "> 30",
+          },
+          {
+            content: "> 20",
+            value: "> 20",
+          },
+          {
+            content: "> 10",
+            value: "> 10",
+          },
+          {
+            content: "< 10",
+            value: "< 10",
+          },
+        ]);
+        break;
+      case "salary":
+        setOptionsFilter([
+          {
+            content: "More than 5000$",
+            value: "> 5000",
+          },
+          {
+            content: "More than 4000$",
+            value: "> 4000",
+          },
+          {
+            content: "More than 3000$",
+            value: "> 3000",
+          },
+          {
+            content: "More than 2000$",
+            value: "> 2000",
+          },
+          {
+            content: "More than 1000$",
+            value: "> 1000",
+          },
+          {
+            content: "Less than 1000$",
+            value: "<= 1000",
+          },
+        ]);
+        break;
+      default:
+        setOptionsFilter([
+          {
+            content: "email@domain1.com",
+            value: "@domain1.com",
+          },
+          {
+            content: "email@domain2.com",
+            value: "@domain2.com",
+          },
+          {
+            content: "email@domain3.com",
+            value: "@domain3.com",
+          },
+        ]);
+        break;
+    }
+  }, [option_filter_by]);
 
   useEffect(() => {
     if (data) setFilteredTimesheetsAndEmployeesWithImages(data.employees);
@@ -114,14 +187,25 @@ export default function EmployeesPage() {
           <section>
             <div className="order-section flex j-c-c a-i-c mt-20">
               <label htmlFor="filter_by">Filter By:</label>
-              <select name="filter_by" id="filter_by">
+              <select
+                name="filter_by"
+                id="filter_by"
+                value={option_filter_by}
+                onChange={(e) => {
+                  setOptionFilterBy(e.target.value);
+                }}
+              >
                 <option value="full_name">Full name</option>
                 <option value="salary">Salary</option>
-                <option value="department">Department</option>
+                <option value="email">Email</option>
               </select>
               <label htmlFor="filter">Filter:</label>
               <select name="filter" id="filter">
-                {/* To be filled */}
+                {options_filter.map((option: any) => (
+                  <option value={option.value} key={option.value}>
+                    {option.content}
+                  </option>
+                ))}
               </select>
               <ButtonComponent
                 title="Apply filter search"
