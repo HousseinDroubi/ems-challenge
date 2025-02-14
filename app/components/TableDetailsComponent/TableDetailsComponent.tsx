aeece4f802b27ca1f571bca60ec5beabaf5b004a
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-14 11:17:47 +0200

    feat: implement component style and functionality

--- app/components/TableDetailsComponent/TableDetailsComponent.tsx
+++ app/components/TableDetailsComponent/TableDetailsComponent.tsx
@@ -1,5 +1,10 @@
 import "./TableDetailsComponent.css";
 
-export default function TableDetailsComponent() {
-  return <></>;
+export default function TableDetailsComponent({ icon, content, alt }: any) {
+  return (
+    <div className="flex a-i-c table-details-component-container">
+      <img src={icon} alt={alt} />
+      <h4>{content}</h4>
+    </div>
+  );
 }
