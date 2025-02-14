e4e3f0383e28bcfb347bdc80e6f89c9b10c879ac
Houssein Droubi <112481387+HousseinDroubi@users.noreply.github.com>
2025-02-14 20:16:52 +0200

    feat: implement search by name, salary and more

--- app/routes/employees._index/route.tsx
+++ app/routes/employees._index/route.tsx
@@ -5,7 +5,7 @@ import { getDB } from "~/db/getDB";
 import { convertToBase64 } from "~/functions/file";
 import "./route.css";
 import SearchBarComponent from "~/components/SearchBarComponent/SearchBarComponent";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export async function loader() {
   const db = await getDB();
@@ -19,6 +19,35 @@ export async function loader() {
 export default function EmployeesPage() {
   const { employees } = useLoaderData();
   const [search_bar_text, setSearchBarText] = useState("");
+  const [
+    filtered_times_and_employees_with_images,
+    setFilteredTimesheetsAndEmployeesWithImages,
+  ] = useState([]);
+  useEffect(() => {
+    if (search_bar_text == "") {
+      setFilteredTimesheetsAndEmployeesWithImages(employees);
+    } else {
+      const arr = employees.filter(
+        (timesheetAndEmployee: any) =>
+          timesheetAndEmployee.full_name
+            .toLowerCase()
+            .includes(search_bar_text.toLowerCase()) ||
+          String(timesheetAndEmployee.salary).includes(search_bar_text) ||
+          String(timesheetAndEmployee.phone_number).includes(search_bar_text) ||
+          timesheetAndEmployee.job_level
+            .toLowerCase()
+            .includes(search_bar_text.toLowerCase()) ||
+          timesheetAndEmployee.department
+            .toLowerCase()
+            .includes(search_bar_text.toLowerCase()) ||
+          timesheetAndEmployee.job_title
+            .toLowerCase()
+            .includes(search_bar_text.toLowerCase())
+      );
+      setFilteredTimesheetsAndEmployeesWithImages(arr);
+    }
+  }, [search_bar_text]);
+
   return (
     <div>
       <NavBarComponent
@@ -34,7 +63,7 @@ export default function EmployeesPage() {
           setSearchBarText={setSearchBarText}
         />
         <article className="employees-container flex w-100">
-          {employees.map((employee: any) => (
+          {filtered_times_and_employees_with_images.map((employee: any) => (
             <EmployeeComponent employee={employee} key={employee.id} />
           ))}
         </article>
